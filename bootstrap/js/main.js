051d4c17f919be95f4f9c9cf0c9619b685db84b1
U-AGFAHEALTHCARE\awwyy <awwyy@WTLLA129.agfahealthcare.com>
2013-08-19 13:13:38 -0400

    some changes

--- bootstrap/js/main.js
+++ bootstrap/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	csapp = new CSApp();
 
 	$('#mainNav li').on('click', csapp.navigate);
-	$('.navbar-brand').on('click', csapp.showHome);
+	$('.navbar-brand').on('click', csapp.navigate);
 
 	csapp.showHome();
 	$('#courtain').fadeOut(1000);
@@ -17,40 +17,36 @@ var CSApp = function() {
 
 CSApp.prototype = function() {
 
+	var navigate = function( event ) {
 
-	var showHome = function() {
-		$('#csmain').empty();
-		$('.navbar-nav li').removeClass('active');
-		$('#csmain').append('<img class="center img-responsive" src="img/home.png"/>');
-	},
-
-	navigate = function( event ) {
 		var caller = this;
+
 		$('.navbar-nav li').removeClass('active');
-		$(caller).addClass('active');
+		$(this).addClass('active');
+		
 		$('#courtain').fadeIn(1000, function() {
 			
 			$('#csmain').empty();
-			var section = $(caller).text();
-			switch(section) {
+
+			switch($(caller).text()) {
 				case 'Ears':
 					csapp.showEars();
 					break;
 				case 'Brain':
 					csapp.showBrain();
-					break
+					break;
 				case 'Mouth':
 					csapp.showMouth();
-					break
-
+					break;
 				case 'Info':
 					csapp.showInfo();
-					break
+					break;
 				default:
+					csapp.showHome();
 					break;
 			}
-			console.log('fadeout');
-			$('#courtain').fadeOut(1000);
+
+			
 		});
 	},
 
@@ -58,28 +54,36 @@ CSApp.prototype = function() {
 		$('#csmain').empty();
 		$('.navbar-nav li').removeClass('active');
 		$('#csmain').append('<img class="center img-responsive" src="img/home.png"/>');
+		$('#courtain').fadeOut(1000);
 	},
 
 	showEars = function() {
-		// $('#csmain').append('<h3>The Sofa Kings</h3>');
-		// $('#csmain').append('<div id="TSKContent"></div>');
-		// $('#TSKContent').text('The Sofa Kings are the next generation of psychedelic jam music. Taking inspiration from classic Funk and Jazz as well as modern improvisational and jam bands, they weave deceptively fluid bass lines with funky and hypnotic drum beats into atmospheric keys and guitar lines that bubble and simmer with rhythm and melody, until they erupt into a soaring celebration of music.');
-
-		$.get('content/tsk.html', function(data) {
+		// $.get('content/tsk.html', function(data) {
+		// 	$('#csmain').html(data);
+		// 	$('#courtain').fadeOut(1000);
+		// });
+		// 
+		$.ajax({
+			url: "content/tsk.html",
+		}).done(function ( data ) {
 			$('#csmain').html(data);
+			$('#courtain').fadeOut(1000);
 		});
 	},
 
 	showBrain = function() {
 		console.log('show brain');
+		$('#courtain').fadeOut(1000);
 	},
 
 	showMouth = function() {
 		console.log('show mouth');
+		$('#courtain').fadeOut(1000);
 	},
 
 	showInfo = function() {
 		console.log('show Info');
+		$('#courtain').fadeOut(1000);
 	};
 
 	return {
